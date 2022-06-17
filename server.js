7101cdfe0be6bfc464b58ff12128e2ba482a9cb4
David Aydin <david.aydin6@gmail.com>
2022-06-17 16:54:14 -0400

    required dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+// Import Dependencies and Json Files
+const express = require("express");
+const cors = require("cors")
+
+// Create App Object
+// Cors Middleware
+// Home route to test app
+// /projects route to retreive proj info
+// /about route to retreive about me
+// setup server listener
